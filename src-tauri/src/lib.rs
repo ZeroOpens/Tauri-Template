d88fb6fa0ed908baf1c685645e3b43adc65536ed
zero-open <2578597860@qq.com>
2025-10-01 19:32:35 +0800

    chore(rust): 规范后端项目结构

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,19 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// 声明子模块
+pub mod error;
+pub mod handlers;
+pub mod models;
+pub mod services;
+pub mod utils;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // 注册插件
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // 注册命令处理器
+        .invoke_handler(tauri::generate_handler![])
+        // 运行
         .run(tauri::generate_context!())
+        // 处理错误
         .expect("error while running tauri application");
 }
